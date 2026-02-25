e39fc356224e5270a99e8e3fafb97b992bd2b45c
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-02-25 18:14:45 +0000

    FI0-10898 Catch Certificate : Multiple Pages breaching SLA during performance testing

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,20 +1,36 @@
 import { SessionStoreFactory } from "../session_store/factory";
 import { getRedisOptions } from "../session_store/redis";
-import { IStoreable } from "../session_store/storeable";
+import { IStorage, IStoreable } from "../session_store/storeable";
 
 
-export default interface Notification extends IStoreable {
+export interface Notification extends IStoreable {
   title: string,
   message: string,
   isPublished: boolean
 }
 
 export default class NotificationService {
+  private static _sessionStore: IStorage<Notification> | null = null;
 
-  public static async get(): Promise<Notification> {
-    const sessionStore = await SessionStoreFactory.getSessionStore(getRedisOptions());
+  public static clearSessionStoreCacheForTests(): void {
+    NotificationService._sessionStore = null;
+  }
 
-    return await sessionStore.read<Notification>('notification');
+  private static async getSessionStore(): Promise<IStorage<Notification>> {
+    let sessionStore = NotificationService._sessionStore;
+
+    if (!sessionStore) {
+      sessionStore = await SessionStoreFactory.getSessionStore(getRedisOptions()) as IStorage<Notification>;
+      NotificationService._sessionStore = sessionStore;
+    }
+
+    return sessionStore;
+  }
+
+  public static async get(): Promise<Notification | null> {
+    const sessionStore = await NotificationService.getSessionStore();
+
+    return sessionStore.read<Notification>('notification');
   }
 
 }
