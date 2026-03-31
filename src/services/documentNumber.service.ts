61244a00e1c311f787173a339b834697efaaee76
Mo Nashawati <mo.nashawati@capgemini.com>
2026-03-31 10:09:33 +0100

    fix: replace String#replace() with String#replaceAll() (SonarCloud maintainability)
    Replace .replace() with .replaceAll() across 3 source files where the
    intent is to replace all occurrences. This addresses ~4 SonarCloud code
    smell issues (es2021 readability).
    
    - errorExtractor.ts: comma-to-dot path conversion
    - uploads.controller.ts: empty row filtering
    - documentNumber.service.ts: random ID generation

--- src/services/documentNumber.service.ts
+++ src/services/documentNumber.service.ts
@@ -182,7 +182,7 @@ export default class DocumentNumberService {
 
 function randomId() {
   // taken from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-  return 'xxxxxxxxx'.replace(/[xy]/g, (c) => {
+  return 'xxxxxxxxx'.replaceAll(/[xy]/g, (c) => {
     const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8); // tslint:disable-line
     return v.toString(16).toUpperCase();
   });
