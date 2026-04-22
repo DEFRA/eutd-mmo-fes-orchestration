0473babbfa6274375bd8c56505ba04849e5344f4
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-04-22 13:34:23 +0100

    FI0-11205 Prevent emoji and unsupported character errors

--- src/routes/exporter-validate.jest.spec.ts
+++ src/routes/exporter-validate.jest.spec.ts
@@ -640,5 +640,32 @@ describe('exporter validate routes', () => {
       expect(mockAddExporterDetails).not.toHaveBeenCalled();
       expect(response.statusCode).toBe(400);
     });
+
+    it('should return 400 if country contains emoji characters', async () => {
+      const response = await server.inject({
+        method: "POST",
+        url: "/v1/exporter-validate",
+        headers: {
+          documentnumber: documentNumber,
+        },
+        app: {
+          claims: {
+            sub: 'Bob',
+          },
+        },
+        payload: {
+          postcode: "SE16 5HL",
+          buildingNumber: "2",
+          streetName: "High Street",
+          buildingName: "The Point",
+          townCity: "Newcastle",
+          county: "Tyneside",
+          country: "🇬🇧",
+        },
+      });
+
+      expect(mockAddExporterDetails).not.toHaveBeenCalled();
+      expect(response.statusCode).toBe(400);
+    });
   });
 });
