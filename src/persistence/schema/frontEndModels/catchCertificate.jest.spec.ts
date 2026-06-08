11fb5a981b40528e996db45747c77be8a3572588
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-06-08 12:41:52 +0100

    test case fix

--- src/persistence/schema/frontEndModels/catchCertificate.jest.spec.ts
+++ src/persistence/schema/frontEndModels/catchCertificate.jest.spec.ts
@@ -152,6 +152,7 @@ describe('toFrontEndCatchCert Mapping BackEnd Certificate to frontEnd', () => {
         currentUri: "Test",
         nextUri: "Test"
       },
+      "transportSummary": null,
       transportations: [{
         id: "0",
         vehicle: "truck"
@@ -356,6 +357,7 @@ describe('toFrontEndCatchCert Mapping BackEnd Certificate to frontEnd', () => {
           isoNumericCode: "SP"
         }
       },
+      "transportSummary": "directLanding • United Kingdom • SPAIN",
       transportations: [],
       exportLocation: {
         exportedFrom: "United Kingdom",
