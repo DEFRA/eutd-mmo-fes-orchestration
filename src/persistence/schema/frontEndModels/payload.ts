bcdcee88815e432baab562b31394a7fc23d452b5
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-06-08 12:46:28 +0100

    test case fix

--- src/persistence/schema/frontEndModels/payload.ts
+++ src/persistence/schema/frontEndModels/payload.ts
@@ -155,6 +155,7 @@ export interface CertificateSummary {
   exportPayload: ProductsLanded,
   conservation: Conservation,
   transport?: Transport,
+  transportSummary?: string | null,
   transportations?: CatchCertificateTransport[],
   exportLocation: ExportLocation,
   landingsEntryOption: BackEndModels.LandingsEntryOptions
