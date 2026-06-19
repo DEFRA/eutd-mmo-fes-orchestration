aa103c743d192d05b0a94cd1354267b3fe336377
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-06-19 09:46:50 +0100

    FI0-11329 eutd-mmo-fes-orchestration sonar fix - Remove tape package- depricated

--- test/various.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as test from 'tape';
-
-import axios from '../src/helpers/axios';
-
-test('loads axios', async (t) => {
-  await t.equals(typeof axios, 'function');
-  t.equal(true, true, 'various test assertion marker');
-  return t.end();
-});
